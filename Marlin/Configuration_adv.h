58243893ec5abecc8f86f03c78e63f35ced2c929
Josselin Jacquard <josselin.jacquard@gmail.com>
2020-08-20 14:47:19 +0200

    augmentation du buffer

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1839,14 +1839,14 @@
 #elif ENABLED(SDSUPPORT)
   #define BLOCK_BUFFER_SIZE 16
 #else
-  #define BLOCK_BUFFER_SIZE 32
+  #define BLOCK_BUFFER_SIZE 64
 #endif
 
 // @section serial
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 6
+#define BUFSIZE 4
 
 // Transmission to Host Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
