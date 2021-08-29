9a5a8a73a0c81344c026d373ff73198d89defe43
Josselin Jacquard <josselin.jacquard@gmail.com>
2021-08-29 22:38:38 +0200

    Revert "ajout correction erreur de compilation"
    This reverts commit 721aa7321b6855fd10b9ce08213a17c8b65b8d5f.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1169,7 +1169,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET xyz_pos_t({ 11., 38., -0.55})
+#define NOZZLE_TO_PROBE_OFFSET { 11, 38, -0.55 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
